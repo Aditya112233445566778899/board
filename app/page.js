1e213ee93edc3db887751149dfa3eb1186a9ea68
aditya200 <aditya@revidd.com>
2023-06-07 16:05:21 +0530

    icons added

--- app/page.js
+++ app/page.js
@@ -1,113 +1,144 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import ScheduleCard from "@/components/ScheduleCard";
+import StatusCard from "@/components/StatusCard";
+import Chart from "@/components/Chart";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const statusCards = [
+    {
+      bgColor: "bg-tCardBg",
+      iconName: "card-icon.svg",
+      title: "Total Revenues",
+      value: "$2,129,430",
+    },
+    {
+      bgColor: "bg-tCardBg2",
+      iconName: "transaction-icon.svg",
+      title: "Total Transactions",
+      value: "1,520",
+    },
+    {
+      bgColor: "bg-tCardBg3",
+      iconName: "thumbs-up-icon.svg",
+      title: "Total Likes",
+      value: "9,721",
+    },
+    {
+      bgColor: "bg-tCardBg4",
+      iconName: "users-icon.svg",
+      title: "Total Users",
+      value: "892",
+    },
+  ];
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const scheduleData = [
+    {
+      borderColor: "border-[#9BDD7C]",
+      title: "Meeting with suppliers from Kuta Bali",
+      time: "14.00-15.00",
+      description: "at Sunset Road, Kuta, Bali",
+    },
+    {
+      borderColor: "border-[#6972C3]",
+      title: "Check operation at Giga Factory 1",
+      time: "18.00-20.00",
+      description: "at Central Jakarta",
+    },
+  ];
+
+  const renderTodaySchedule = () => (
+    <div className="bg-white rounded-[20px] py-8 px-10 w-[480px]">
+      <div className="flex w-full items-center">
+        <p className="text-black font-bold text-lg flex-1">Today’s schedule</p>
+        <button className="text-subTitle text-xs flex items-center gap-2">
+          See All
+          <Image
+            alt="icon"
+            src="assets/icons/right-cap-icon.svg"
+            width={5}
+            height={5}
+          />
+        </button>
+      </div>
+      <div className="flex flex-col gap-3 mt-4">
+        {scheduleData.map((item) => (
+          <ScheduleCard props={item} />
+        ))}
+      </div>
+    </div>
+  );
+
+  const renderSearchBar = () => (
+    <div className="flex items-center py-3 px-4 bg-white rounded-[10px] mr-3">
+      <input
+        placeholder="Search..."
+        className="bg-white placeholder:text-placeholderColor text-sm outline-none"
+      />
+      <Image src="assets/icons/search-icon.svg" width={12} height={12} />
+    </div>
+  );
+
+  const renderRightSideHeader = () => (
+    <div className="flex items-center mb-10">
+      <p className="flex-1 font-bold text-black text-xl">Dashboard</p>
+      {renderSearchBar()}
+      <Image
+        src="assets/icons/bell-icon.svg"
+        alt="icon"
+        width={20}
+        height={20}
+        className="mx-5"
+      />
+      <Image
+        src="http://3.bp.blogspot.com/-As-olkkh0no/T1RWk2I53HI/AAAAAAAAQsI/vSU73goD6Jo/s1600/BEN10+18.jpg"
+        alt="icon"
+        width={30}
+        height={30}
+        className="rounded-full aspect-square object-cover"
+      />
+    </div>
+  );
+
+  const renderRightSideCard = () => (
+    <div className="flex items-center justify-between">
+      {statusCards.map(({ bgColor, iconName, title, value }) => (
+        <StatusCard
+          key={title}
+          bgColor={bgColor}
+          iconName={iconName}
+          title={title}
+          value={value}
         />
-      </div>
+      ))}
+    </div>
+  );
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const renderChat = () => (
+    <div>
+      <Chart />
+    </div>
+  );
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const renderRightSideBottomPart = () => (
+    <div className="flex items-center justify-between">
+      {renderTodaySchedule()}
+    </div>
+  );
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  const renderRightSidePart = () => (
+    <div className="flex-1">
+      {renderRightSideHeader()}
+      {renderRightSideCard()}
+      {renderChat()}
+      {renderRightSideBottomPart()}
+    </div>
+  );
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+  return (
+    <main className="p-10 flex items-start gap-[60px] w-full bg-backgroundColor">
+      <Navbar />
+      {renderRightSidePart()}
     </main>
-  )
+  );
 }
