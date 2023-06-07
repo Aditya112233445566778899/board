3de964a6d4e72ac64312c80c608d7ad3cf21acb5
aditya200 <aditya@revidd.com>
2023-06-07 20:12:58 +0530

    improve readability

---
+++ utils/store.js
@@ -0,0 +1,41 @@
+export const statusCards = [
+  {
+    bgColor: "bg-tCardBg",
+    iconName: "card-icon.svg",
+    title: "Total Revenues",
+    value: "$2,129,430",
+  },
+  {
+    bgColor: "bg-tCardBg2",
+    iconName: "transaction-icon.svg",
+    title: "Total Transactions",
+    value: "1,520",
+  },
+  {
+    bgColor: "bg-tCardBg3",
+    iconName: "thumbs-up-icon.svg",
+    title: "Total Likes",
+    value: "9,721",
+  },
+  {
+    bgColor: "bg-tCardBg4",
+    iconName: "users-icon.svg",
+    title: "Total Users",
+    value: "892",
+  },
+];
+
+export const scheduleData = [
+  {
+    borderColor: "border-[#9BDD7C]",
+    title: "Meeting with suppliers from Kuta Bali",
+    time: "14.00-15.00",
+    description: "at Sunset Road, Kuta, Bali",
+  },
+  {
+    borderColor: "border-[#6972C3]",
+    title: "Check operation at Giga Factory 1",
+    time: "18.00-20.00",
+    description: "at Central Jakarta",
+  },
+];
