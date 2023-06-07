81e5c939942783d363ea68263e2667172554ceaa
aditya200 <aditya@revidd.com>
2023-06-07 17:56:24 +0530

    added required colors in the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        backgroundColor: "#DDDDDD",
+        placeholderColor: "#B0B0B0",
+        tCardBg: "#DDEFE0",
+        tCardBg2: "#F4ECDD",
+        tCardBg3: "#EFDADA",
+        tCardBg4: "#DEE0EF",
+        subTitle:"#858585",
+        meetScheduleTitle:"#666666",
+        meetScheduleSubTitle:'#999999'
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+};
