2527d69dcbc75656b6748dfee1e07ddc192ddcac
A_d_i_t_y_a_c_h <109592097+Aditya112233445566778899@users.noreply.github.com>
2023-06-09 16:57:19 +0530

    Update PieChart.jsx

--- components/PieChart.jsx
+++ components/PieChart.jsx
@@ -33,40 +33,42 @@ function PieChart() {
     },
   };
 
-  return (
-    <div
-      id="chart"
-      className="w-full flex items-center sm:justify-around lg:justify-between xl:justify-around"
-    >
-      <ReactApexChart
-        options={chatOptions.options}
-        series={chatOptions.series}
-        type="pie"
-        width={230}
-      />
+  if (typeof window !== "undefined" && window.innerHeight)
+    return (
+      <div
+        id="chart"
+        className="w-full flex items-center sm:justify-around lg:justify-between xl:justify-around"
+      >
+        <ReactApexChart
+          options={chatOptions.options}
+          series={chatOptions.series}
+          type="pie"
+          width={230}
+        />
 
-      <div className="flex flex-col gap-5">
-        {chatOptions.options.labels.map((label, index) => (
-          <div key={label}>
-            <div className="flex gap-2 items-center mb-1">
-              <div
-                style={{
-                  width: "11px",
-                  height: "11px",
-                  borderRadius: "100%",
-                  backgroundColor: chatOptions.options.colors[index],
-                }}
-              />
-              <p className="font-bold text-sm text-black">{label}</p>
+        <div className="flex flex-col gap-5">
+          {chatOptions.options.labels.map((label, index) => (
+            <div key={label}>
+              <div className="flex gap-2 items-center mb-1">
+                <div
+                  style={{
+                    width: "11px",
+                    height: "11px",
+                    borderRadius: "100%",
+                    backgroundColor: chatOptions.options.colors[index],
+                  }}
+                />
+                <p className="font-bold text-sm text-black">{label}</p>
+              </div>
+              <p className="text-xs text-subTitle ml-5">
+                {chatOptions.series[index]}%
+              </p>
             </div>
-            <p className="text-xs text-subTitle ml-5">
-              {chatOptions.series[index]}%
-            </p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  else return null;
 }
 
 export default PieChart;
